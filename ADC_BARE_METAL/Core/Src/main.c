45de0296fff929afd09580a8e5612876990bce33
Marcos-Antunes04 <mca4729@gmail.com>
2024-02-04 20:13:47 -0300

    ADC update 04/02/2024

--- ADC_BARE_METAL/Core/Src/main.c
+++ ADC_BARE_METAL/Core/Src/main.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+void SysClockConfig(void);
+
 int main(void){
 	RCC->APB2ENR |= (1<<8); //Start ADC
 	RCC->AHB1ENR |= (1<<0); //Start GPIOA
@@ -53,4 +55,37 @@ int main(void){
 	while (1){
 
 	}
+}
+
+
+
+
+
+void SysClockConfig(void){
+	RCC->CR |= (1 << 16); //HSE on
+	while(!(RCC->CR & (1<<17)));//Wait for HSE clock ready flag
+
+	RCC->APB1ENR |= (1<<28);
+	PWR-CR |= (1<<14);
+
+	FLASH->ACR = (1<<9) | (1<<10) | (1<<8) | 0X05;
+
+	//AHB PRESCALER
+	CC->CFGR |= 0X00;
+	//APB1 PRESCALER
+	CC->CFGR |= 0x00001400U;
+	//APB2 PRESCALER
+	CC->CFGR |= 0x00008000U;
+
+	//CONFIGURE THE MAIN PLL
+	RCC->PLLCFGR = (4 <<0) | (180 << 6) | (0 <<16) | (1<<22);
+
+	//Enable the PLL and wait for it to become ready
+	RCC->CR |= (1<<24);
+	while (!(RCC->CR & (1<<25)));
+
+	//Select the Clock Source and wait for it to be set
+	RCC->CFGR |= 0x02;
+	while ((RCC->CFGR & (3<<2)) != 0x08);
+
 }
--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## TIPS
 
 ```sh
-   To use printf() -> #include <stdio.h> and press red button on botton right corner
+   To use printf() -> #include <stdio.h> and press red button on bottom right corner
 ```
 
  ![IMAGEM 1](.Imagens/stm1.png)
